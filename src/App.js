6a9f7539dea9965ce72222bed0516f88c583db61
C.O. Jansen <ophe.jansen@gmail.com>
2026-05-08 18:12:26 +0200

    select items + display recipe

--- src/App.js
+++ src/App.js
@@ -1,49 +1,90 @@
+import { useState } from "react";
+import { Dropdown, ButtonGroup } from "react-bootstrap";
+
 import "./App.css";
-import RESOURCES, { BASE_URL } from "./data/resources";
+import ITEMS from "./data/items";
 import Header from "./components/Header.js";
+import Recipe from "./components/Recipe.js";
 
 function App() {
-  const miningResources = RESOURCES.filter(resource => resource.obtainBy === "mining");
-  const smeltingResources = RESOURCES.filter(resource => resource.obtainBy === "smelting");
-  const atmosphericCondenserResources = RESOURCES.filter(
-    resource => resource.obtainBy === "atmosphericCondenser",
-  );
-  const chemistryLabResources = RESOURCES.filter(resource => resource.obtainBy === "chemistryLab");
+  const [itemsList, setItemsList] = useState(ITEMS);
+  const [selectedItem, setSelectedItem] = useState("");
+  const [search, setSearch] = useState("");
 
-  const renderResourcesList = (title, resourcesList) => {
-    return (
-      <>
-        <h2>{title}</h2>
-        <ul>
-          {resourcesList.map((resource, index) => {
-            return (
-              <li key={index}>
-                <img
-                  src={BASE_URL + resource.logo + `.png`}
-                  width="25"
-                  alt={`${resource.name} icon`}
-                />
-                {resource.name}
-              </li>
-            );
-          })}
-        </ul>
-      </>
-    );
+  const itemListOptions = () => {
+    return itemsList.map((item, index) => {
+      return (
+        <Dropdown.Item key={item.name + index} eventKey={item.name}>
+          {item.name}
+        </Dropdown.Item>
+      );
+    });
+  };
+
+  const handleItemOptionChange = e => {
+    const newItemsList = ITEMS.filter(item => item.name.indexOf(e.target.value) !== -1);
+
+    setItemsList(newItemsList);
+    setSearch(e.target.value);
+  };
+
+  const handleDropdownSelect = (eventKey, _e) => {
+    const currentItem = ITEMS.filter(item => item.name === eventKey)[0];
+    setSelectedItem(currentItem);
+  };
+
+  const handleClearForm = () => {
+    setSelectedItem("");
   };
 
   return (
-    <div>
-      <div className="container">
-        <Header />
+    <div className="container">
+      <Header />
 
-        <main>
-          {renderResourcesList("Mining", miningResources)}
-          {renderResourcesList("Smelting", smeltingResources)}
-          {renderResourcesList("Atmospheric Condenser", atmosphericCondenserResources)}
-          {renderResourcesList("Chemistry Lab", chemistryLabResources)}
-        </main>
-      </div>
+      <main className="row">
+        <h2>What do you want to create ?</h2>
+
+        <form className="col-md-4">
+          <div className="mb-3">
+            <ButtonGroup className="col-12">
+              <Dropdown as={ButtonGroup} onSelect={handleDropdownSelect}>
+                <Dropdown.Toggle variant="primary">Select an item to create</Dropdown.Toggle>
+                <Dropdown.Menu className="mt-1">
+                  <input
+                    type="text"
+                    placeholder="Search..."
+                    className="rounded mx-2"
+                    onChange={handleItemOptionChange}
+                    value={search}
+                    autoFocus
+                    style={{
+                      width: "calc(100% - 20px)",
+                    }}
+                  />
+                  {itemListOptions()}
+                </Dropdown.Menu>
+              </Dropdown>
+            </ButtonGroup>
+
+            {selectedItem && (
+              <p className="col badge text-bg-secondary">
+                {selectedItem.name}{" "}
+                <i
+                  className="fa-solid fa-x"
+                  style={{ cursor: "pointer" }}
+                  onClick={handleClearForm}
+                ></i>
+              </p>
+            )}
+          </div>
+        </form>
+
+        <section className="col-12">
+          <h3>Recipe</h3>
+
+          {selectedItem ? <Recipe item={selectedItem} /> : <p>Nothing here.</p>}
+        </section>
+      </main>
     </div>
   );
 }
