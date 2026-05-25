d37f1e16b13663b73820059173075b4f934da9ee
C.O. Jansen <ophe.jansen@gmail.com>
2026-05-25 16:11:09 +0200

    add planetsList for resource detail page

--- src/pages/ResourcePage.tsx
+++ src/pages/ResourcePage.tsx
@@ -1,15 +1,15 @@
 import { Suspense } from "react";
-import { Await, Link, useRouteLoaderData } from "react-router-dom";
+import { Await, useRouteLoaderData } from "react-router-dom";
 
-import { OBTAIN_BY, RESOURCES_BASE_URL } from "../util/constants";
+import { OBTAIN_BY } from "../util/constants";
 import { fetchPlanets, fetchResource, fetchResources } from "../util/http";
 import { toCapitalizeCase } from "../util/utils";
 import { ResourceType } from "../types/resourceType";
-import { PlanetType } from "../types/planetType";
 
 import RecipeTree from "../components/RecipeTree";
 import DetailHeader from "../components/shared/DetailHeader";
 import DetailContent from "../components/shared/DetailContent";
+import PlanetsList from "../components/resource/PlanetsList";
 
 const ResourcePage = (): React.JSX.Element => {
   const { resource, resources } = useRouteLoaderData("resource");
@@ -28,49 +28,23 @@ const ResourcePage = (): React.JSX.Element => {
                   <p>
                     Obtain by : {toCapitalizeCase(OBTAIN_BY[loadedResource?.obtainBy || 0].from)}
                   </p>
-                  {loadedResource.planets && (
-                    <Suspense>
-                      <Await resolve={planetsLoader(loadedResource.planets)}>
-                        {(loadData: any) => {
-                          const planets = loadData.planets;
 
-                          if (planets.length === 7) return <p>Available on all planets.</p>;
-
-                          return (
-                            <>
-                              <h3>Found on the following planets :</h3>
-                              {planets.map((planet: PlanetType) => {
-                                return (
-                                  <div key={planet.id}>
-                                    <Link to={`/planets/${planet.id}`}>
-                                      <img
-                                        src={RESOURCES_BASE_URL + planet.icon}
-                                        style={{ width: "40px", height: "40px" }}
-                                        className="me-2"
-                                        alt={planet.name}
-                                      />
-                                      {toCapitalizeCase(planet.name)}
-                                    </Link>
-                                  </div>
-                                );
-                              })}
-                            </>
-                          );
-                        }}
-                      </Await>
-                    </Suspense>
-                  )}
+                  <PlanetsList resource={loadedResource} />
                 </DetailContent>
 
-                <Suspense fallback={<p>Loading resources...</p>}>
-                  <Await resolve={resources}>
-                    {(loadedData: { resources: ResourceType[] }) => {
-                      return (
-                        <RecipeTree element={loadedResource} resources={loadedData.resources} />
-                      );
-                    }}
-                  </Await>
-                </Suspense>
+                {loadedResource.recipe ? (
+                  <Suspense fallback={<p>Loading full recipe...</p>}>
+                    <Await resolve={resources}>
+                      {(loadedData: { resources: ResourceType[] }) => {
+                        return (
+                          <RecipeTree element={loadedResource} resources={loadedData.resources} />
+                        );
+                      }}
+                    </Await>
+                  </Suspense>
+                ) : (
+                  ""
+                )}
               </>
             );
           }}
@@ -90,7 +64,7 @@ const resourcesLoader = async () => {
   return await fetchResources();
 };
 
-const planetsLoader = async (planets: string[]) => {
+export const planetsLoader = async (planets: string[]) => {
   return await fetchPlanets(planets);
 };
 
