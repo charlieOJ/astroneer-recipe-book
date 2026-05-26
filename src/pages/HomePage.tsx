d4f864e22efa3f5e70ccdd668f856739e1675582
C.O. Jansen <ophe.jansen@gmail.com>
2026-05-26 17:23:30 +0200

    new homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,36 +1,21 @@
-import { Suspense } from "react";
-import { Await, useLoaderData } from "react-router-dom";
-
-import { ItemType } from "../types/itemType";
-import { fetchItems } from "../util/http";
-
-import Item from "../components/Item";
-import SearchableList from "../components/SearchableList";
+import { Link } from "react-router-dom";
 
 const HomePage = (): React.JSX.Element => {
-  const { items } = useLoaderData();
-
   return (
     <>
-      <h2>Items</h2>
-
-      <Suspense fallback={<p className="text-center">Loading items...</p>}>
-        <Await resolve={items}>
-          {(loadedItems: ItemType[]) => {
-            return (
-              <SearchableList items={loadedItems} itemKeyFn={(item: ItemType) => item.id}>
-                {(item: ItemType) => <Item item={item} />}
-              </SearchableList>
-            );
-          }}
-        </Await>
-      </Suspense>
+      <main className="main-bg d-flex align-items-center justify-content-around">
+        <Link to="resources" className="btn btn-lg btn-light btn-outline-dark">
+          Resources
+        </Link>
+        <Link to="items" className="btn btn-lg btn-light btn-outline-dark">
+          Items
+        </Link>
+        <Link to="planets" className="btn btn-lg btn-light btn-outline-dark">
+          Planets
+        </Link>
+      </main>
     </>
   );
 };
 
 export default HomePage;
-
-export const itemsLoader = async () => {
-  return await fetchItems();
-};
