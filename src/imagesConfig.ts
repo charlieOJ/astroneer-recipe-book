60dcdd5c990c44e5380bd9c8524343fc9ea42202
C.O. Jansen <ophe.jansen@gmail.com>
2026-06-05 20:04:28 +0200

    comment img config for now

--- src/imagesConfig.ts
+++ src/imagesConfig.ts
@@ -10,33 +10,33 @@ const normalizeKey = (key: any) => {
 
 const allImages = {} as any;
 
-// Load images for /resources
-const resourcesContext = require.context(
-  "./assets/images/resources",
-  false,
-  /\.(jpg|jpeg|png|webp)$/,
-);
-resourcesContext.keys().forEach(key => {
-  const name = normalizeKey(key);
+// // Load images for /resources
+// const resourcesContext = require.context(
+//   "./assets/images/resources",
+//   false,
+//   /\.(jpg|jpeg|png|webp)$/,
+// );
+// resourcesContext.keys().forEach(key => {
+//   const name = normalizeKey(key);
 
-  allImages[name] = resourcesContext(key);
-});
+//   allImages[name] = resourcesContext(key);
+// });
 
-// Load images for /items
-const itemsContext = require.context("./assets/images/items", false, /\.(jpg|jpeg|png|webp)$/);
-itemsContext.keys().forEach(key => {
-  const name = normalizeKey(key);
+// // Load images for /items
+// const itemsContext = require.context("./assets/images/items", false, /\.(jpg|jpeg|png|webp)$/);
+// itemsContext.keys().forEach(key => {
+//   const name = normalizeKey(key);
 
-  allImages[name] = itemsContext(key);
-});
+//   allImages[name] = itemsContext(key);
+// });
 
-// Load images for /planets
-const planetsContext = require.context("./assets/images/planets", false, /\.(jpg|jpeg|png|webp)$/);
-planetsContext.keys().forEach(key => {
-  const name = normalizeKey(key);
+// // Load images for /planets
+// const planetsContext = require.context("./assets/images/planets", false, /\.(jpg|jpeg|png|webp)$/);
+// planetsContext.keys().forEach(key => {
+//   const name = normalizeKey(key);
 
-  allImages[name] = planetsContext(key);
-});
+//   allImages[name] = planetsContext(key);
+// });
 
 // Load images for /hazards
 const hazardsContext = require.context("./assets/images/hazards", false, /\.(jpg|jpeg|png|webp)$/);
