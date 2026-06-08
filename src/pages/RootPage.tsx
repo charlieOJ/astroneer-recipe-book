1cc774b0f710bf19e6a8ef20b70ad006c4e915ff
C.O. Jansen <ophe.jansen@gmail.com>
2026-06-09 00:26:40 +0200

    add i18next + i18n + add change locale dropdown + add lang in path handle

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,7 +1,12 @@
 import { Outlet } from "react-router-dom";
+
+import { useLanguageFromUrl } from "../hooks/useLanguageFromUrl";
+
 import Header from "../components/Header";
 
 const RootPage = (): React.JSX.Element => {
+  useLanguageFromUrl();
+
   return (
     <>
       <div className="container">
